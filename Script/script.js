3fb0a63bdb3a01926d9b7ae3bc8423b9ee28294c
Geman <gemfru@yahoo.com>
2020-06-19 23:42:20 +0800

    Fixing console error

--- Script/script.js
+++ Script/script.js
@@ -1,7 +1,13 @@
 (function(){
 
     function Start(){
+
+        let title = document.title
+        let aboutButt = document.getElementById("AboutButton")
+
+        aboutButt.addEventListener("click", ()=> console.log("About button was clicked"))
         console.log("App started")
+        console.log("TItle" + title)
     }
 
     window.onload = Start
